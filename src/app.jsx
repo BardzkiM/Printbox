351b403c543566734a0d4ca804f0e1b7f3307c04
BardzkiM <michalbardzki@gmail.com>
2016-07-20 01:25:04 +0200

    top menu + filter menu + ProductListContainer

--- src/app.jsx
+++ src/app.jsx
@@ -1,17 +1,35 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Hello from "./components/Hello";
 import Head from "./components/head/Head";
-
-
+import Menu from "./components/menu/Menu";
+import FilterMenu from "./components/filter/FilterMenu";
+import ProductListContainer from "./components/products/ProductListContainer";
 
 export class App extends React.Component {
 
 	render() {
+
+		let menuTabs = [
+			{text: "PHOTOBOOKS", target: "#"},
+			{text: "CALENDARS", target: "#"},
+			{text: "CANVAS", target: "#"},
+			{text: "CARDS", target: "#"},
+			{text: "GIFTS STATIONERY", target: "#"}
+		];
+
 		return (
 			<div className="asd">
 				<Head />
+				<Menu tabsSource={menuTabs}/>
+				<div className="content">
+					<div className="filters col-2">
+						<FilterMenu />
+					</div>
+					<div className="right-content col-10">
 
+						<ProductListContainer />
+					</div>
+				</div>
 			</div>
 		);
 	}
--- src/components/head/Head.jsx
+++ src/components/head/Head.jsx
@@ -7,8 +7,8 @@ export default class Hello extends React.Component {
 	render() {
 		return(
 			<div className="head col-12">
-				<h1 className="logo col-6">Printbox</h1>
-				<div className="col-6"><HeadMenu /></div>
+				<h1 className="logo col-7">Printbox</h1>
+				<div className="col-5"><HeadMenu /></div>
 			</div>
 		);
 	}
